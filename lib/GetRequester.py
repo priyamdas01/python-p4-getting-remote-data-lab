8d93dd1df933f5eaa8521aab06f20eafbac3f237
Priyam Das <domidas01@gmail.com>
2023-08-19 15:39:28 -0400

    complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,18 @@
 import requests
 import json
 
+
 class GetRequester:
 
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        return requests.get(self.url).content
 
     def load_json(self):
-        pass
+        json_data = json.loads(self.get_response_body())
+        return json_data
+
+
+print(GetRequester("http://data.cityofnewyork.us/resource/uvks-tn5n.json").load_json())
